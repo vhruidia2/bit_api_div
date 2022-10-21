051b6d2ab160d616707f0bc96cb4a3e668ee0220
vhruidia2 <vhruidia2@gmail.com>
2022-10-20 19:26:27 -0500

    condicional de 0

--- app.js
+++ app.js
@@ -3,30 +3,27 @@ const express = require("express")
 const app = express()
 const port = 5000
 
-//rutas
+
 app.get('/api', (req, res) => {
 
     return res.status(200).json({message: "Bienvenido al Api :)"})
 
 })
 
-//suma
+
 app.get('/division/:num1/:num2', (req, res) => {
 
-    let result = parseInt(req.params.num1) / parseInt(req.params.num2) //NaN
-    console.log(req.params)
+    if( parseInt(req.params.num1)===0 || parseInt(req.params.num2)===0){
+    
+        return res.status(400).json({message:"No se puede dividir entre 0"})
+    } else {
+        let result= parseInt(req.params.num1) / parseInt(req.params.num2) 
+    
+        console.log(req.params)
 
     return res.status(200).json({result: result})
-
-})
-
-app.get('/division', (req, res) => {
-    // /suma?num1=25&&num2=25
-    let result = parseInt(req.query.num1) + parseInt(req.query.num2) //NaN
-    console.log(req.query)
-
-    return res.status(200).json({result: result})
-
+    
+    }
 })
 
 app.listen(port, () => console.log(`Escuchando el puerto ${port}`))
